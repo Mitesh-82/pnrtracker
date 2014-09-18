93d72332d83082fd8d5b3ab2a21af0172a050f93
Mitesh Patel <miteshpatel.tech@gmail.com>
2014-09-18 17:23:27 +0900

    pnrtracker-8 Make Custom List Elements

--- app/src/main/java/com/droidsoft/pnrtracker/database/SyncInterface.java
+++ app/src/main/java/com/droidsoft/pnrtracker/database/SyncInterface.java
@@ -8,20 +8,9 @@ import java.util.Set;
  * Created by mitesh.patel on 18-09-2014.
  */
 public abstract class SyncInterface {
+    protected List<SyncListener> syncListeners;
     private boolean isSyncInProgress = false;
     private Set<String> pendingPnr;
-    protected List<SyncListener> syncListeners;
-
-    public class SyncIntervals {
-        public static final int EVERY_15_MINUTES = 15;
-        public static final int EVERY_30_MINUTES = 30;
-        public static final int EVERY_1_HOUR = 60 * 1;
-        public static final int EVERY_3_HOUR = 60 * 3;
-        public static final int EVERY_DAY = 60 * 24;
-
-        public static final int MAX_INTERVAL = EVERY_DAY;
-        public static final int NEVER = Integer.MAX_VALUE;
-    }
 
     protected SyncInterface() {
         syncListeners = new ArrayList<SyncListener>();
@@ -39,6 +28,8 @@ public abstract class SyncInterface {
 
     public abstract void doTimedSync(int interval);
 
+    public abstract void doServerRequest(String pnrNo);
+
     public class SyncIntervals {
         public static final int EVERY_15_MINUTES = 15;
         public static final int EVERY_30_MINUTES = 30;
@@ -49,8 +40,6 @@ public abstract class SyncInterface {
         public static final int MAX_INTERVAL = EVERY_DAY;
         public static final int NEVER = Integer.MAX_VALUE;
     }
-
-    public abstract void doServerRequest(String pnrNo);
 
 
 }
