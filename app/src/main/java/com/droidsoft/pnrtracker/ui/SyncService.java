86d8209a2ce2f193b2101b72e6dd93e7b47fa926
mitesh.patel <mitesh.patel@lge.com>
2014-12-31 13:59:47 +0530

    pnrtracker-23 Source Upgrade :: Studio 1.0

--- app/src/main/java/com/droidsoft/pnrtracker/ui/SyncService.java
+++ app/src/main/java/com/droidsoft/pnrtracker/ui/SyncService.java
@@ -14,6 +14,7 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 public class SyncService extends Service {
+    
     // constant
     public static final long NOTIFY_INTERVAL = SyncInterface.SyncIntervals.EVERY_15_MINUTES; // 15 minutes
     private static boolean isCreated = false;
